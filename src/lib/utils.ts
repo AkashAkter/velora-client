69d98ef13423e0cc883b40a833fa698c21e2f0b4
AkashAkter <akash31akter@gmail.com>
2025-03-02 22:25:35 +0600

    minor change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -26,7 +26,7 @@ export function cleanParams(params: Record<string, any>): Record<string, any> {
   return Object.fromEntries(
     Object.entries(params).filter(
       (
-        [_, value]
+        [_, value] // eslint-disable-line @typescript-eslint/no-unused-vars
       ) =>
         value !== undefined &&
         value !== "any" &&
